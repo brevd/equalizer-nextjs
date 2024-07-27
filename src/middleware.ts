b75749ba4e92ea91277c0e7556104452c6a58641
Broderick-mint <brode.dejonge12@gmail.com>
2024-07-27 14:13:29 -0400

    update middleware to allow sign-in and up

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,19 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-const isPublicRoute = createRouteMatcher(["/", "/onboarding"]);
+const isPublicRoute = createRouteMatcher([
+  "/",
+  "/onboarding",
+  "/(sign-in|sign-up)(.*)",
+]);
 
 export default clerkMiddleware((auth, req) => {
   const { userId, sessionClaims, redirectToSignIn } = auth();
 
   // If the user isn't signed in and the route is private, redirect to sign-in
   if (!userId && !isPublicRoute(req)) {
-    return redirectToSignIn({ returnBackUrl: req.url });
+    const signinUrl = new URL("/sign-in", req.url);
+    return NextResponse.redirect(signinUrl);
   }
 
   // Catch users who do not have `onboardingComplete: true` in their publicMetadata
